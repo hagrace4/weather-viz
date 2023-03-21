7c88452ffcc960f88ec65f8389c6f631c5e5f3a6
hagrace4 <hagrace4@gmail.com>
2023-03-21 12:45:41 -0700

    update to use openweather API 3.0

--- app.js
+++ app.js
@@ -15,20 +15,20 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-// Replace YOUR_API_KEY with the API key you obtained from OpenWeatherMap
 const apiKey = config.apiKey;
 
-app.get('/weather/:city', (req, res) => {
-  const city = req.params.city;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+async function fetchWeatherData(lat, lon, exclude = 'minutely,alerts') {
+  const apiKey = config.apiKey;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}`;
 
-  request(url, (error, response, body) => {
-    if (error) {
-      res.status(500).send('Error fetching weather data');
-    } else {
-      const weatherData = JSON.parse(body);
-      res.send(weatherData);
-    }
-  });
-});
+  try {
+    const response = await axios.get(url);
+    const weatherData = response.data;
+    console.log(weatherData);
+    return weatherData;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+}
+
+fetchWeatherData(34.23, -118.52);
